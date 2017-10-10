d788c42e059d443420697df767faaf44c2c3e89f
Jakie Rice <jakie.rice@gmail.com>
2017-10-10 14:49:13 -0500

    Fix User model associate error

--- models/user.js
+++ models/user.js
@@ -34,6 +34,10 @@ module.exports = function (sequelize, DataTypes) {
 			allowNull: true
 		}
 	});
+
+	User.associate =(models) => {
+		User.hasMany(models.Decision);
+	};
 	// Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
 	User.prototype.validPassword = function (password) {
 		return bcrypt.compareSync(password, this.password);
@@ -43,10 +47,6 @@ module.exports = function (sequelize, DataTypes) {
 	User.hook("beforeCreate", function (user) {
 		user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
 	});
-
-	User.associate = function(models) {
-		User.hasMany(models.Decision);
-	};
 
 	return User;
 };
