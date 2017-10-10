8cf14945f27b8b6acad8a59eff09a689411b5c37
Charles McDowell <charlesmcdowell@charless-mbp.attlocal.net>
2017-10-10 12:55:34 -0500

    Add GET /api/profile route

--- controllers/dd_controller.js
+++ controllers/dd_controller.js
@@ -32,25 +32,22 @@ module.exports = function (app) {
 		});
 	});
 
-	// // Route for logging user out
-	// app.get("/logout", function (req, res) {
-	// 	req.logout();
-	// 	res.redirect("/");
-	// });
+	// Route for logging user out
+	app.get("/logout", function (req, res) {
+		req.logout();
+		res.redirect("/");
+	});
 
-	// // Route for getting some data about our user to be used client side
-	// app.get("/api/user_data", function (req, res) {
-	// 	if (!req.user) {
-	// 		// The user is not logged in, send back an empty object
-	// 		res.json({});
-	// 	}
-	// 	else {
-	// 		// Otherwise send back the user's email and id
-	// 		// Sending back a password, even a hashed password, isn't a good idea
-	// 		res.json({
-	// 			email: req.user.email,
-	// 			id: req.user.id
-	// 		});
-	// 	}
-	// });
+	// Route for getting some data about our user to be used client side
+	app.get("/api/profile", function (req, res) {
+		if (!req.user) {
+			// The user is not logged in, send back an empty object
+			res.json({});
+		}
+		else {
+			db.User.findOne({ where: {email: req.user.email}}).then((dbUser) => {
+				res.status(200).send(dbUser);
+			});
+		}
+	});
 };
