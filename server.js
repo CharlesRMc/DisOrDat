12a68c86bb5d5ad8ef5a1e2131666548c36a53f1
Jakie Rice <jakie.rice@gmail.com>
2017-10-11 21:31:49 -0500

    Add main.hbs view and start feed.hbs view

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 // Requiring passport as we've configured it
 const passport = require('./config/passport');
+const exphbs = require('express-handlebars');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -14,6 +15,9 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: '8913iohlkao9c8h1;39h;g9avj81h9', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -21,7 +25,11 @@ app.use(passport.session());
 
 // Requiring our routes
 require('./controllers/html-routes.js')(app);
-require('./controllers/dd_controller.js')(app);
+// require('./controllers/dd_controller.js')(app);
+
+var routes = require("./controllers/dd_controller");
+
+app.use("/", routes);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then( () => {
